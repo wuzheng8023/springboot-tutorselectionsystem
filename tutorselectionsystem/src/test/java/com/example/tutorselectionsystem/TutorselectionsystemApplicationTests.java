37d6fc7c5f8e5552859bbb309d5e9e7af6125734
wuzheng <1345381396@qq.com>
2020-03-12 12:01:15 +0800

    导师选择系统项目主体部分

---
+++ tutorselectionsystem/src/test/java/com/example/tutorselectionsystem/TutorselectionsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tutorselectionsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorselectionsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
